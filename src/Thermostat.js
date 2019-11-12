0cddf96cceed78d114079711a35f178ac715a1c5
Lena Krasnova <olkras03@gmail.com>
2019-11-12 15:51:33 +0000

    all test passing

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -33,3 +33,7 @@ Thermostat.prototype.isPowerSavingModeOn = function() {
 Thermostat.prototype.switchPowerSavingModeOff = function() {
  this.powerSavingMode = false;
 }
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+  this.powerSavingMode = true;
+ }
